bafb43403c6ab647adb1cbd5f7a08f0806ce311f
NadavHrd <nadav.howard.haifa@gmail.com>
2025-07-05 23:30:01 +0300

    Added a function that reads images data.

--- ImageHashEncrypt/Encrypter.cpp
+++ ImageHashEncrypt/Encrypter.cpp
@@ -3,3 +3,18 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+/*
+This function gets an image path and returns a struct which includes the image data.
+Input: const std::string& imagePath - the path to the image that will be read.
+Output: ImageData - the data of the given image.
+Runtime complexity: O(n).
+*/
+ImageData Encrypter::readImage(const std::string& imagePath)
+{
+	ImageData image;
+	int channels = 0;
+	
+	image.pixels = stbi_load(imagePath.c_str(), &image.width, &image.height, &channels, RGB_CHANNELS_VALUE); // Load the given image
+
+	return image; // In case the image path is invalid, image.pixels will be nullptr
+}
--- ImageHashEncrypt/Encrypter.h
+++ ImageHashEncrypt/Encrypter.h
@@ -2,10 +2,31 @@
 
 #include <iostream>
 
+constexpr int RGB_CHANNELS_VALUE = 3;
+
+typedef struct ImageData
+{
+	int width;
+	int height;
+	unsigned char* pixels;
+
+	// Default values
+	ImageData(int width = 0, int height = 0, unsigned char* pixels = nullptr) : width(width), height(height), pixels(pixels) {};
+} ImageData;
+
 class Encrypter
 {
 public:
 	Encrypter() {}; // C'tor
 
 	~Encrypter() {}; // D'tor
+
+private:
+	/*
+	This function gets an image path and returns a struct which includes the image data.
+	Input: const std::string& imagePath - the path to the image that will be read.
+	Output: ImageData - the data of the given image.
+	Runtime complexity: O(n).
+	*/
+	static ImageData readImage(const std::string& imagePath);
 };
