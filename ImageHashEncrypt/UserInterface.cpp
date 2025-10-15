b78ea06b3b86010d56e371faa23f1f518cd05631
NadavHrd <nadav.howard.haifa@gmail.com>
2025-10-15 23:04:27 +0300

    Added a function for the user to decrypt images.

--- ImageHashEncrypt/UserInterface.cpp
+++ ImageHashEncrypt/UserInterface.cpp
@@ -43,7 +43,7 @@ void UserInterface::userEncrypt()
 
 	// Getting all of the fields needed for the encryption from the user
 
-	std::cout << "Enter the path for the image you want to encrypt: ";
+	std::cout << RESET << "Enter the path for the image you want to encrypt: ";
 
 	std::cin >> imageToEncryptPath;
 
@@ -60,28 +60,102 @@ void UserInterface::userEncrypt()
 	resCode = Encrypter::EncryptImage(imageToEncryptPath, encryptedImagePath, key); // Encrypting the image
 
 	// Printing the encryption result
+	
+	std::cout << BOLD;
+	
 	switch (resCode)
 	{
 		case SUCCESS:
-			std::cout << "Image was encrypted successfully.\n";
+			std::cout << GREEN << "Image was encrypted successfully.\n";
 			break;
 		case IMAGE_PATH_INVALID:
-			std::cout << "ERROR: The given image path is invalid.\n";
+			std::cout << RED << "ERROR: The given image path is invalid.\n";
 			break;
 		case DST_IMAGE_PATH_ALREADY_EXISTS:
-			std::cout << "ERROR: The given .txt file already exist.\n";
+			std::cout << RED << "ERROR: The given .txt file already exist.\n";
 			break;
 		case DST_IMAGE_PATH_NOT_TEXT_FILE:
-			std::cout << "ERROR: The given destination file is not .txt.\n";
+			std::cout << RED << "ERROR: The given destination file is not .txt.\n";
 			break;
 		case ERROR_CREATING_ENCRYPTED_IMAGE:
-			std::cout << "ERROR: An error occurred while trying to create a .txt file.\n";
+			std::cout << RED << "ERROR: An error occurred while trying to create a .txt file.\n";
 			break;
 		case ERROR_HASHING_IMAGE_VALUES:
-			std::cout << "ERROR: The given image data is corrupted.\n";
+			std::cout << RED << "ERROR: The given image data is corrupted.\n";
 			break;
 		case KEY_INVALID:
-			std::cout << "ERROR: Invalid key. A valid key does not include whitespace and is 4 - 32 chars long.\n";
+			std::cout << RED << "ERROR: Invalid key. A valid key does not include whitespace and is 4 - 32 chars long.\n";
+			break;
+	}
+
+	UserInterface::waitForContinue();
+}
+
+/*
+This function lets the user decrypt an image. The function will then print the decryption result to the user.
+Input: none.
+Output: none.
+Runtime complexity: O(n).
+*/
+void UserInterface::userDecrypt()
+{
+	std::string encryptedImagePath = "";
+	std::string destImagePath = "";
+	std::string key = "";
+	int resCode = 0;
+
+	UserInterface::clearScreen();
+
+	// Get the required inputs for the decryption from the user
+
+	std::cout << RESET << "Enter the path for the encrypted image (.txt): ";
+
+	std::cin >> encryptedImagePath;
+
+	std::cout << "\n\nEnter the path which you would like to save the new image at (.png): ";
+
+	std::cin >> destImagePath;
+
+	std::cout << "\n\nEnter the decryption key: ";
+
+	std::cin >> key;
+
+	std::cout << "\n\n\nDecrypting image. This may take a while...\n";
+
+	resCode = Decrypter::decryptImage(encryptedImagePath, key, destImagePath); // Decrypting the image
+
+	// Printing the decryption result
+
+	std::cout << BOLD;
+
+	switch (resCode)
+	{
+		case DECRYPTION_SUCCESS:
+			std::cout << GREEN << "Image was decrypted successfully.\n";
+			break;
+		case SRC_IMAGE_NOT_TXT:
+			std::cout << RED << "ERROR: The given encrypted image is not a .txt file.\n";
+			break;
+		case INVALID_SRC_IMAGE_PATH:
+			std::cout << RED << "ERROR: The given encrypted image path is invalid.\n";
+			break;
+		case INVALID_KEY_GIVEN:
+			std::cout << RED << "ERROR: Invalid key. A valid key does not include whitespace and is 4 - 32 chars long.\n";
+			break;
+		case WRONG_IMAGE_DIMENSIONS:
+			std::cout << RED << "ERROR: The given encrypted image dimensions are corrupted.\n";
+			break;
+		case INVALID_DEST_IMAGE_PATH:
+			std::cout << RED << "ERROR: The given .png path to save the image at is invalid.\n";
+			break;
+		case DEST_IMAGE_NOT_PNG:
+			std::cout << RED << "ERROR: The destination path to save the image at is not .png.\n";
+			break;
+		case DEST_IMAGE_ALREADY_EXISTS:
+			std::cout << RED << "ERROR: The destination path to save the image at already exists.\n";
+			break;
+		case ERROR_CREATING_IMAGE:
+			std::cout << RED << "ERROR: An error occurred while trying to create the image (.png).\n";
 			break;
 	}
 
--- ImageHashEncrypt/UserInterface.h
+++ ImageHashEncrypt/UserInterface.h
@@ -5,6 +5,34 @@
 #include "Encrypter.h"
 #include "Decrypter.h"
 
+// Reset
+constexpr const char* RESET = "\033[0m";
+
+// Regular colors
+constexpr const char* BLACK = "\033[30m";
+constexpr const char* RED = "\033[31m";
+constexpr const char* GREEN = "\033[32m";
+constexpr const char* YELLOW = "\033[33m";
+constexpr const char* BLUE = "\033[34m";
+constexpr const char* MAGENTA = "\033[35m";
+constexpr const char* CYAN = "\033[36m";
+constexpr const char* WHITE = "\033[37m";
+
+// Bright colors
+constexpr const char* BRIGHT_BLACK = "\033[90m";
+constexpr const char* BRIGHT_RED = "\033[91m";
+constexpr const char* BRIGHT_GREEN = "\033[92m";
+constexpr const char* BRIGHT_YELLOW = "\033[93m";
+constexpr const char* BRIGHT_BLUE = "\033[94m";
+constexpr const char* BRIGHT_MAGENTA = "\033[95m";
+constexpr const char* BRIGHT_CYAN = "\033[96m";
+constexpr const char* BRIGHT_WHITE = "\033[97m";
+
+// Text styles
+constexpr const char* BOLD = "\033[1m";
+constexpr const char* UNDERLINE = "\033[4m";
+constexpr const char* REVERSED = "\033[7m";
+
 class UserInterface
 {
 public:
@@ -15,6 +43,14 @@ public:
 	Runtime complexity: O(n).
 	*/
 	void userEncrypt();
+
+	/*
+	This function lets the user decrypt an image. The function will then print the decryption result to the user.
+	Input: none.
+	Output: none.
+	Runtime complexity: O(n).
+	*/
+	void userDecrypt();
 
 private:
 	/*
